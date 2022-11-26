93866c840fc5025ac922ef509e7c3f4534eab71d
kamila <58568615+kamilarakhimova@users.noreply.github.com>
2022-11-26 19:25:32 +0000

    Update main.s

--- main.s
+++ main.s
@@ -1,5 +1,6 @@
 # Целочисленные параметры в функции передаются в регистрах rdi, rsi, rdx, rcx, r8, r9, 
-# а возвращаемое целочисленное значение передаётся в регистре rax (и в регистре rdx, если оно занимает более 64 байт)
+# а возвращаемое целочисленное значение передаётся в регистре rax (и в регистре rdx, если оно занимает более 64 байт).
+# Возвращаемое дробное значение передаётся в регистре xmm0.
 
 	.file	"main.c"						# имя файла, который компилировался
 	.intel_syntax noprefix						# использование синтаксиса в стиле Intel
@@ -10,7 +11,7 @@ f:									# непосредственно функция f
 	movapd	xmm1, xmm0						# xmm1 = xmm0 = x, выполняется процессором x86
 	mulsd	xmm0, xmm0						# xmm0 = xmm0 * xmm0 = x * x
 	mulsd	xmm0, xmm1						# xmm0 = xmm0 * xmm1 = x * x * x
-	ret								# выход из функции, возвращаем значение в xmm0 = x * x * x
+	ret								# выход из функции f, возвращаем значение в xmm0 = x * x * x
 	.globl	integral						# объявление и экспортирование вовне символа integral
 	.type	integral, @function					# отмечаем, что integral -- это функция
 integral:								# непосредственно функция integral
@@ -32,7 +33,7 @@ integral:								# непосредственно функция integral
 	movsd	QWORD PTR 32[rsp], xmm4					# QWORD PTR 32[rsp] = n2
 	movsd	QWORD PTR 40[rsp], xmm1					# QWORD PTR 40[rsp] = h
 .L4:
-	pxor	xmm0, xmm0
+	pxor	xmm0, xmm0						# xmm0 = 0
 	cvtsi2sd	xmm0, ebp					# xmm0 = i - 1
 	mulsd	xmm0, QWORD PTR 40[rsp]					# xmm0 = xmm0 * h = (i - 1) * h
 	add	ebp, 1							# ebp = ebp + 1 = i + 1
@@ -47,75 +48,75 @@ integral:								# непосредственно функция integral
 	movsd	xmm4, QWORD PTR 40[rsp]					# xmm4 = h
 	mov	r12d, 20						# r12d = 20 = n
 	mulsd	xmm4, xmm0						# xmm4 = xmm4 * xmm0 = h * I2n
-	pxor	xmm0, xmm0
-	subsd	xmm0, xmm4						# xmm0 = xmm0 - xmm4 = xmm0 - I2n * h
-	andpd	xmm0, XMMWORD PTR .LC2[rip]
+	pxor	xmm0, xmm0						# xmm0 = 0
+	subsd	xmm0, xmm4						# xmm0 = xmm0 - xmm4 = In - I2n
+	andpd	xmm0, XMMWORD PTR .LC2[rip]				# fabs(In - I2n)
 	movsd	QWORD PTR 48[rsp], xmm4					# In = xmm4 = I2n
-	comisd	xmm0, QWORD PTR 56[rsp]					# сравниваем xmm0 и error <=> сравниваем (In - I2n) и error
+	comisd	xmm0, QWORD PTR 56[rsp]					# сравниваем xmm0 и error <=> сравниваем fabs(In - I2n) и error
 	jb	.L19							# если меньше, то переходим на .L19
 .L9:
-	add	r12d, r12d
-	pxor	xmm0, xmm0
-	movq	xmm4, r14
-	cvtsi2sd	xmm0, r12d
-	divsd	xmm4, xmm0
-	movsd	QWORD PTR 40[rsp], xmm4
-	test	r12d, r12d
-	jle	.L11
-	lea	ebp, -1[r12]
-	mov	r13d, -1
-	pxor	xmm1, xmm1
+	add	r12d, r12d						# r12d = r12d + r12d = n * 2
+	pxor	xmm0, xmm0						# xmm0 = 0
+	movq	xmm4, r14						# xmm4 = r14 = b - a
+	cvtsi2sd	xmm0, r12d					# xmm0 = n
+	divsd	xmm4, xmm0						# xmm4 = (b - a) / n
+	movsd	QWORD PTR 40[rsp], xmm4					# QWORD PTR 40[rsp] = h = xmm4 = (b - a) / n
+	test	r12d, r12d						# сраваниваем r12d = n и 0
+	jle	.L11							# если меньше/равно, то переходим на .L11
+	lea	ebp, -1[r12]						# ebp = n
+	mov	r13d, -1						# r13d = -1
+	pxor	xmm1, xmm1						# xmm1 = 0
 .L8:
-	pxor	xmm0, xmm0
-	movsd	QWORD PTR 8[rsp], xmm1
-	cvtsi2sd	xmm0, r13d
-	mulsd	xmm0, QWORD PTR 40[rsp]
-	add	r13d, 1
-	addsd	xmm0, QWORD PTR 16[rsp]
-	call	rbx
-	mulsd	xmm0, QWORD PTR 32[rsp]
-	movsd	xmm1, QWORD PTR 8[rsp]
-	addsd	xmm0, QWORD PTR 24[rsp]
-	addsd	xmm1, xmm0
-	cmp	ebp, r13d
-	jne	.L8
+	pxor	xmm0, xmm0						# xmm0 = 0
+	movsd	QWORD PTR 8[rsp], xmm1					# I2n = QWORD PTR 8[rsp] = xmm1 = 0
+	cvtsi2sd	xmm0, r13d					# xmm0 = i - 1
+	mulsd	xmm0, QWORD PTR 40[rsp]					# xmm0 = xmm0 * h = (i - 1) * h
+	add	r13d, 1							# r13d += 1
+	addsd	xmm0, QWORD PTR 16[rsp]					# xmm0 = xmm0 + a = a + (i - 1) * h
+	call	rbx							# вызываем rbx = f
+	mulsd	xmm0, QWORD PTR 32[rsp]					# xmm0 = n2 * xmm0 = n2 * f(a + (i - 1) * h)
+	movsd	xmm1, QWORD PTR 8[rsp]					# xmm1 = I2n
+	addsd	xmm0, QWORD PTR 24[rsp]					# xmm0 = n1 + xmm0 = n1 + n2 * f(a + (i - 1) * h)
+	addsd	xmm1, xmm0						# xmm1 = xmm1 + xmm0 = I2n + n1 + n2 * f(a + (i - 1) * h)
+	cmp	ebp, r13d						# сравниваем ebp и r13d <=> n и i
+	jne	.L8							# если не равны, то переходим на .L8
 .L7:
-	mulsd	xmm1, QWORD PTR 40[rsp]
-	movsd	xmm0, QWORD PTR 48[rsp]
-	subsd	xmm0, xmm1
-	andpd	xmm0, XMMWORD PTR .LC2[rip]
-	comisd	xmm0, QWORD PTR 56[rsp]
-	jb	.L3
-	movsd	QWORD PTR 48[rsp], xmm1
-	jmp	.L9
+	mulsd	xmm1, QWORD PTR 40[rsp]					# xmm1 *= QWORD PTR 40[rsp] <=> I2n *= h
+	movsd	xmm0, QWORD PTR 48[rsp]					# xmm0 = In
+	subsd	xmm0, xmm1						# xmm0 -= xmm1 <=> In - I2n
+	andpd	xmm0, XMMWORD PTR .LC2[rip]				# fabs(xmm0)
+	comisd	xmm0, QWORD PTR 56[rsp]					# сравниваем xmm0 и error <=> сравниваем fabs(In - I2n) и error
+	jb	.L3							# если меньше, то переходим на .L3
+	movsd	QWORD PTR 48[rsp], xmm1					# QWORD PTR 48[rsp] = In
+	jmp	.L9							# переходим (прыгаем) на метку .L9
 .L19:
-	movapd	xmm1, xmm4
+	movapd	xmm1, xmm4						# xmm1 = xmm4 = (b - a) / n
 .L3:
-	add	rsp, 64
-	movapd	xmm0, xmm1
+	add	rsp, 64							# возвращаем стеку его 64 байта
+	movapd	xmm0, xmm1						# xmm0 = xmm1 = I2n
 	pop	rbx
 	pop	rbp
 	pop	r12
 	pop	r13
 	pop	r14
-	ret
+	ret								# выходим из функции integral, возвращаем значение в xmm0 = I2n
 .L11:
-	pxor	xmm1, xmm1
-	jmp	.L7
+	pxor	xmm1, xmm1						# xmm1 = 0
+	jmp	.L7							# переходим (прыгаем) на метку .L7
 .LC3:
-	.string	"rw+"
+	.string	"rw+"							# метка с "rw+" для вызова fopen для входного файла потом
 .LC4:
-	.string	"generator"
+	.string	"generator"						# метка с "generator" для получения входных данных через генерацию тестов потом
 .LC5:
-	.string	"Error! Try again, please."
+	.string	"Error! Try again, please."				# метка с "Еrror! Try again, please." для вывода сообщения об ошибке потом
 .LC8:
-	.string	"%lf %lf %lf %lf"
+	.string	"%lf %lf %lf %lf"					# метка с "%lf %lf %lf %lf" для вызова fprintf с результатами генерации данных потом
 .LC9:
-	.string	"%lf"
+	.string	"%lf"							# метка с "%lf" для вызова fscanf, fprintf с сообщением о результате потом
 .LC11:
-	.string	"Elapsed: %ld ns"
+	.string	"Elapsed: %ld ns"					# метка с "Elapsed: %ld ns" для вызова printf c сообщением о замере времени потом
 .LC12:
-	.string	"w+"
+	.string	"w+"							# метка с "w+" для вызова fopen для выходного файла потом
 	.globl	main							# объявление и экспортирование вовне символа main
 	.type	main, @function						# отмечаем, что main -- это функция
 main:									# непосредственно функция main
